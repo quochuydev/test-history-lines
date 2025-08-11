c60b0c4116d0efdda0d590d06411733c040533a5
quochuydev <quochuy.dev@gmail.com>
2025-08-11 17:02:56 +0700

    init

---
+++ main.ts
@@ -0,0 +1,4 @@
+console.log("This application is for testing history lines");
+console.log("Input: Folder with JSON files are history of crypto prices");
+console.log("Output: Are dates of buy and sell to get best profit");
+console.log("Usage: npm run dev");
