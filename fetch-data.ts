a3582c8b67d47b3dc3d84ed8766a993e5bfd8912
quochuydev <quochuy.dev@gmail.com>
2025-08-11 18:00:57 +0700

    update

--- fetch-data.ts
+++ fetch-data.ts
@@ -3,50 +3,68 @@ import * as path from "path";
 import { FetchConfig, getTodayString, generateFileName } from "./utils";
 
 const data: FetchConfig[] = [
-  {
-    symbol: "BTCUSDT",
-    interval: "1d",
-    limit: 365, // 365 days
-  },
-  {
-    symbol: "BTCUSDT",
-    interval: "1h",
-    limit: 1 * 24 * 90, // 90 days
-  },
-  {
-    symbol: "ETHUSDT",
-    interval: "1d",
-    limit: 365, // 365 days
-  },
+  // // BTC
+  // {
+  //   symbol: "BTCUSDT",
+  //   interval: "1d",
+  //   limit: 365, // 365 days
+  // },
+  // {
+  //   symbol: "BTCUSDT",
+  //   interval: "1h",
+  //   limit: 1 * 24 * 90, // 90 days
+  // },
+  // // SOL
+  // {
+  //   symbol: "SOLUSDT",
+  //   interval: "1d",
+  //   limit: 365, // 365 days
+  // },
+  // {
+  //   symbol: "SOLUSDT",
+  //   interval: "1d",
+  //   limit: 30, // 30 days
+  // },
+  // {
+  //   symbol: "SOLUSDT",
+  //   interval: "1d",
+  //   limit: 60, // 60 days
+  // },
+  // {
+  //   symbol: "SOLUSDT",
+  //   interval: "1d",
+  //   limit: 90, // 90 days
+  // },
+  // {
+  //   symbol: "SOLUSDT",
+  //   interval: "15m",
+  //   limit: 15 * 4 * 24 * 30, // 30 days
+  // },
+  // ETH
   {
     symbol: "ETHUSDT",
     interval: "15m",
     limit: 15 * 4 * 24 * 30, // 30 days
   },
   {
-    symbol: "SOLUSDT",
-    interval: "1d",
-    limit: 365, // 365 days
-  },
-  {
-    symbol: "SOLUSDT",
+    symbol: "ETHUSDT",
     interval: "1d",
     limit: 30, // 30 days
   },
   {
-    symbol: "SOLUSDT",
+    symbol: "ETHUSDT",
     interval: "1d",
     limit: 60, // 60 days
   },
   {
-    symbol: "SOLUSDT",
+    symbol: "ETHUSDT",
     interval: "1d",
     limit: 90, // 90 days
   },
   {
-    symbol: "SOLUSDT",
-    interval: "15m",
-    limit: 15 * 4 * 24 * 30, // 30 days
+    symbol: "ETHUSDT",
+    interval: "1d",
+    limit: 365, // 365 days
   },
 ];
 
--- main.ts
+++ main.ts
@@ -5,25 +5,66 @@ import { FetchConfig, generateFileName } from "./utils";
 const INVESTMENT_AMOUNT = 100; // Default investment amount in USD
 
 const data: FetchConfig[] = [
-  {
-    symbol: "BTCUSDT",
-    interval: "1d",
-    limit: 365, // 365 days
-  },
-  {
-    symbol: "BTCUSDT",
-    interval: "1h",
-    limit: 1 * 24 * 90, // 90 days
-  },
-  {
-    symbol: "ETHUSDT",
-    interval: "1d",
-    limit: 365, // 365 days
-  },
+  // {
+  //   symbol: "BTCUSDT",
+  //   interval: "1d",
+  //   limit: 365, // 365 days
+  // },
+  // {
+  //   symbol: "BTCUSDT",
+  //   interval: "1h",
+  //   limit: 1 * 24 * 90, // 90 days
+  // },
+  // SOL
+  // {
+  //   symbol: "SOLUSDT",
+  //   interval: "1d",
+  //   limit: 365, // 365 days
+  // },
+  // {
+  //   symbol: "SOLUSDT",
+  //   interval: "1d",
+  //   limit: 30, // 30 days
+  // },
+  // {
+  //   symbol: "SOLUSDT",
+  //   interval: "1d",
+  //   limit: 60, // 60 days
+  // },
+  // {
+  //   symbol: "SOLUSDT",
+  //   interval: "1d",
+  //   limit: 90, // 90 days
+  // },
+  // {
+  //   symbol: "ETHUSDT",
+  //   interval: "15m",
+  //   limit: 15 * 4 * 24 * 30, // 30 days
+  // },
   {
     symbol: "ETHUSDT",
     interval: "15m",
     limit: 15 * 4 * 24 * 30, // 30 days
+  },
+  {
+    symbol: "ETHUSDT",
+    interval: "1d",
+    limit: 30, // 30 days
+  },
+  {
+    symbol: "ETHUSDT",
+    interval: "1d",
+    limit: 60, // 60 days
+  },
+  {
+    symbol: "ETHUSDT",
+    interval: "1d",
+    limit: 90, // 90 days
+  },
+  {
+    symbol: "ETHUSDT",
+    interval: "1d",
+    limit: 365, // 365 days
   },
 ];
 
